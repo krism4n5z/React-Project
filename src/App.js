7bbbc81810b76e4fd5c503807a5d79468e4d84cf
kris_man <m4n5zu@gmail.com>
2019-10-08 18:50:19 +0200

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
+        <NavBar />
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
-        </Router>
-      </div>
+        </Router>{" "}
+      </div>{" "}
     </ThemeContext.Provider>
   );
 };
