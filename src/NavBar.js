b76e0473c4df0213e7e7a5cf0a896e7f09ef9c6d
kris_man <m4n5zu@gmail.com>
2019-10-08 18:54:36 +0200

    adding navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-import NavBar = () => (
-    <header
-        css={css`
-            background-color: #333;
-            padding: 15px;
-        `}
-        >
-        <Link to="/">Adopt Me!</Link>
-        <span role="img" aria-label="logo">🍼 </span>
-    </header>
-)
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: #333;
+      padding: 15px;
+    `}
+  >
+    <Link to="/">Adopt Me!</Link>
+    <span role="img" aria-label="logo">
+      🍼
+    </span>
+  </header>
+);
 
 export default NavBar;
